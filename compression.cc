1f4c145428633ce92cd5db2b13105022a2c451eb
Michael Stramel <m.stramel89@gmail.com>
2011-07-11 08:45:33 -0500

    Reads in and Ouputs the full file size now

--- compression.cc
+++ compression.cc
@@ -18,7 +18,7 @@ void ReadInputFile (char *buffer, int size, char *input) {
 
 void WriteOutputFile (char *buffer, char *output, int size) {
   ofstream out (output, ios::binary);
-  out.write(output, size);
+  out.write(buffer, size);
   out.close();
   return;
 }
@@ -38,15 +38,16 @@ void Predictor (char *input_buffer, char *output_buffer, int size) {
 }
  
 void ComputeResidual  (char *input, char *output, int size) {
-  char *input_buffer = new char [size*size];
-  char *output_buffer = new char [size*size];
-  ReadInputFile (input_buffer, size, input);
+  int size_of_file = size * size;
+  char *input_buffer = new char [size_of_file];
+  char *output_buffer = new char [size_of_file];
+  ReadInputFile (input_buffer, size_of_file, input);
   Predictor (input_buffer, output_buffer, size);
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
       output_buffer[i*size+j] -= input_buffer[i*size+j];
     }
   }
-  WriteOutputFile (output_buffer, output, size);
+  WriteOutputFile (output_buffer, output, size_of_file);
   return;
 }
